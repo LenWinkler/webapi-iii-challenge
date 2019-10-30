00b15131663c9ad8621561f1478eaa1f8b13bc9e
LenWinkler <lenwinkler86@gmail.com>
2019-10-30 17:31:36 -0400

    wrote validatePost middlewares

--- users/userRouter.js
+++ users/userRouter.js
@@ -61,7 +61,13 @@ function validateUser(req, res, next) {
 };
 
 function validatePost(req, res, next) {
-
+    if(!req.body) {
+        res.status(400).json({ message: "Missing post data" })
+    } else if(!req.body.text) {
+        res.status(400).json({ message: "Missing required text field" })
+    } else {
+        next();
+    }
 };
 
 module.exports = router;
